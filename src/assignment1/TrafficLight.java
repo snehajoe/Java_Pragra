1b77d9b78c6c779ef3a2d0b6d729bed01b6d8a99
snehajoe <joesnehat@gmail.com>
2024-04-12 14:40:45 -0400

    assignemnt

---
+++ src/assignment1/TrafficLight.java
@@ -0,0 +1,32 @@
+package assignment1;
+import java.util.Scanner;
+
+public class TrafficLight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the traffic light color (red, yellow, or green): ");
+        String input = scanner.nextLine().toLowerCase();
+
+        String action;
+        switch (input) {
+            case "red":
+                action = "Stop";
+                System.out.println("The traffic light is " + input + ", you should " + action + ".");
+                break;
+            case "yellow":
+                action = "Prepare to stop";
+                System.out.println("The traffic light is " + input + ", you should " + action + ".");
+                break;
+            case "green":
+                action = "Go";
+                System.out.println("The traffic light is " + input + ", you should " + action + ".");
+                break;
+            default:
+                System.out.println("Invalid input");
+                break;
+        }
+
+        scanner.close();
+    }
+}
