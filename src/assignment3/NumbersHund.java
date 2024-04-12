02582a11e4fc71a67a3d613340d86a1e32e061b8
snehajoe <joesnehat@gmail.com>
2024-04-12 14:58:06 -0400

    assignment

---
+++ src/assignment3/NumbersHund.java
@@ -0,0 +1,10 @@
+package assignment3;
+public class NumbersHund {
+    public static void main(String[] args) {
+        int n=1;
+        while(n<=100){
+            System.out.println(n);
+            n++;
+        }
+    }
+}
