cf2764bdb545d69726e86b2c2aea37072adf186b
snehajoe <joesnehat@gmail.com>
2024-04-15 20:33:14 -0400

    files added

---
+++ src/assignment5/Palindrome.java
@@ -0,0 +1,26 @@
+package assignment5;
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the string: ");
+        String s= scanner.nextLine();
+        s = s.toLowerCase();
+        char[] chars = s.toCharArray();
+
+        int left = 0;
+        int right = chars.length - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                System.out.println("The string is not a palindrome");
+                return;
+            }
+            left++;
+            right--;
+        }
+        System.out.println("The string is a palindrome");
+    }
+}
