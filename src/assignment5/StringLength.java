d71d594e239076af3877687c082544fdd9780482
snehajoe <joesnehat@gmail.com>
2024-04-17 14:25:26 -0400

    files added

--- src/assignment5/StringLength.java
+++ src/assignment5/StringLength.java
@@ -7,15 +7,28 @@ public class StringLength {
 
         System.out.println("Enter the string: ");
         String input = scanner.nextLine();
-        String[] words = input.split(" ");
 
-        StringLength s = new StringLength();
-        s.subStringLength(words);
+        int longestSubstringLength= substringLength(input);
+        System.out.println("Length of the longest substring without repeating characters: " + longestSubstringLength);
 
     }
 
-    public static void subStringLength(String[] words){
+    public static int substringLength(String s){
+        int n = s.length();
+        int left=0, right = 0;
+        int maxLength = 0;
 
-
+        while (right < n) {
+            for (int i = left; i < right; i++) {
+                if (s.charAt(i) == s.charAt(right)) {
+                    left = i + 1;
+                    break;
+                }
+            }
+            maxLength = Math.max(maxLength, right-left+1);
+            right++;
+        }
+        return maxLength;
     }
 }
+
